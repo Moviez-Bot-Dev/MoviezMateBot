e7ae0cf5014712eac9424680fe4895b4e52e1910
Moviez Bot Dev <143684485+Moviez-Bot-Dev@users.noreply.github.com>
2023-12-21 23:23:22 +0530

    Add files via upload

---
+++ server/__init__.py
@@ -0,0 +1,2 @@
+
+# I have my dirty mind @Mania24SupportBot 😎😍
---
+++ server/exceptions.py
@@ -0,0 +1,8 @@
+
+    # 🥰  Thank you for helping  this journey  @Mania24SupportBot
+
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
