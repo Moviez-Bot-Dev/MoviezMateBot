9dd4aedd08894fc1616356b28f55023cbde3431c
Moviez Bot Dev <143684485+Moviez-Bot-Dev@users.noreply.github.com>
2023-12-24 00:06:30 +0530

    Add files via upload

--- util/render_template.py
+++ util/render_template.py
@@ -12,7 +12,7 @@ import aiohttp
 
 
 async def render_page(id, secure_hash, src=None):
-    file = await Moviezमंत्रBot.get_messages(int(LOG_CHANNEL), int(id))
+    file = await MoviezMateBot.get_messages(int(LOG_CHANNEL), int(id))
     file_data = await get_file_ids(MoviezMateBot, int(LOG_CHANNEL), int(id))
     if file_data.unique_id[:6] != secure_hash:
         logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
